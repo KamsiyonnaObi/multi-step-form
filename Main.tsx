fe3b7c4730f842a50d55e7369f53344b021522f4
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-02-20 23:10:56 -0600

    chore: build mobile header and footer

--- Main.tsx
+++ Main.tsx
@@ -1,7 +1,32 @@
 import React from "react";
 import { createRoot } from "react-dom/client";
 import App from "./src/App";
+import { Button, ThemeProvider, createTheme } from "@mui/material";
 
 const container = document.getElementById("app");
 const root = createRoot(container!);
-root.render(<App />);
+
+const theme = createTheme({
+  components: {
+    MuiButton: {
+      defaultProps: {
+        disableRipple: true,
+        disableElevation: true,
+      },
+      styleOverrides: {
+        root: {
+          fontFamily: "Ubuntu",
+          padding: "8px 12px",
+          color: "var(--alabaster)",
+          backgroundColor: "var(--marine-blue)",
+          textTransform: "none",
+        },
+      },
+    },
+  },
+});
+root.render(
+  <ThemeProvider theme={theme}>
+    <App />
+  </ThemeProvider>
+);
--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,42 @@
 import React from "react";
 import Paper from "@mui/material/Paper";
+import Button from "@mui/material/Button";
 
 const App = () => {
   return (
     <>
-      <Paper elevation={2}>
-        <h1>Personal info</h1>
+      <div>
+        <img src="../assets/images/bg-sidebar-mobile.svg" alt="header-image" />
+      </div>
+      <Paper elevation={2} sx={{ margin: "0 24px" }}>
+        <section className="step-container">
+          <h1>Personal info</h1>
+          <p className="grey-text">
+            Please provide your name, email address, and phone number.
+          </p>
+        </section>
       </Paper>
+      {/* Mobile Footer */}
+      <div className="mobile-footer">
+        <Button
+          variant="text"
+          onClick={() => {
+            alert("I've been clicked");
+          }}
+          sx={{ display: "none" }}
+        >
+          Go Back
+        </Button>
+        <Button
+          variant="contained"
+          onClick={() => {
+            alert("I've been clicked");
+          }}
+          sx={{ marginLeft: "auto" }}
+        >
+          Next Step
+        </Button>
+      </div>
     </>
   );
 };
