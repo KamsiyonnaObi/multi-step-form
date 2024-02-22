56501a4789a87a7566b5436b69902c8610b660fa
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-02-21 18:15:37 -0600

    chore: stage 1 build form

--- src/App.tsx
+++ src/App.tsx
@@ -11,12 +11,24 @@ const App = () => {
       </div>
       <section className="form-container">
         <Stage />
-        <Paper elevation={2} sx={{ margin: "0 24px" }}>
+        <Paper sx={{ margin: "0 24px", borderRadius: "8px" }}>
           <section className="step-container">
             <h1>Personal info</h1>
             <p className="grey-text">
               Please provide your name, email address, and phone number.
             </p>
+            <form style={{ display: "flex", flexDirection: "column" }}>
+              <label htmlFor="name">Name</label>
+              <input id="name" type="text" placeholder="e.g. Stephen King" />
+              <label htmlFor="email">Email Address</label>
+              <input
+                id="email"
+                type="text"
+                placeholder="e.g. stephenking@lorem.com"
+              />
+              <label htmlFor="phone">Phone Number</label>
+              <input id="phone" type="text" placeholder="e.g. +1 234 567 890" />
+            </form>
           </section>
         </Paper>
       </section>
