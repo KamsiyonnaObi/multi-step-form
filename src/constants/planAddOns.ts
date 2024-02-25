ed6882211002a15082b6dad529a6606d996c46ad
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-02-25 11:00:12 -0600

    extract: plan data from constants

---
+++ src/constants/planAddOns.ts
@@ -0,0 +1,21 @@
+// Data for each plan add on
+export const planAddOns = [
+  {
+    id: 1,
+    name: "Online service",
+    price: 1,
+    desc: "Access to multiplayer games",
+  },
+  {
+    id: 2,
+    name: "Larger storage",
+    price: 2,
+    desc: "Extra 1TB of cloud save",
+  },
+  {
+    id: 3,
+    name: "Customizable profile",
+    price: 2,
+    desc: "Custom theme on your profile",
+  },
+];
---
+++ src/constants/plans.ts
@@ -0,0 +1,21 @@
+// Data for each plan
+export const plans = [
+  {
+    id: 1,
+    name: "Arcade",
+    price: 9,
+    image: "../../../assets/images/icon-arcade.svg",
+  },
+  {
+    id: 2,
+    name: "Advanced",
+    price: 12,
+    image: "../../../assets/images/icon-advanced.svg",
+  },
+  {
+    id: 3,
+    name: "Pro",
+    price: 15,
+    image: "../../../assets/images/icon-pro.svg",
+  },
+];
