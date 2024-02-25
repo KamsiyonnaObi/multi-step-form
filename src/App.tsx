46f982188167a4b855efe5d38bc6f387988ef493
Kamsiyonna <86691010+KamsiyonnaObi@users.noreply.github.com>
2024-02-25 14:33:20 -0600

    chore: complete form

--- src/App.tsx
+++ src/App.tsx
@@ -3,14 +3,19 @@ import Paper from "@mui/material/Paper";
 import Button from "@mui/material/Button";
 import Stage from "./components/stages/Stage";
 import FormStep from "./components/FormStep";
+import { Confirm } from "./components/confirm/Confirm";
 
 const App = () => {
+  const [isFormComplete, setIsFormComplete] = React.useState(false);
   const [currentStep, setCurrentStep] = React.useState(1);
   const handleNextStep = () => {
     setCurrentStep((prev) => prev + 1);
   };
   const handlePrevStep = () => {
     setCurrentStep((prev) => prev - 1);
+  };
+  const handleConfirm = () => {
+    setIsFormComplete(true);
   };
   return (
     <>
@@ -19,41 +24,49 @@ const App = () => {
       </div>
       <section className="form-container">
         <Stage stage={currentStep} />
+
         <Paper sx={{ margin: "0 20px", borderRadius: "8px" }}>
           <section className="step-container">
-            <FormStep currentStep={currentStep} />
+            {isFormComplete ? (
+              <Confirm />
+            ) : (
+              <FormStep currentStep={currentStep} />
+            )}
           </section>
         </Paper>
       </section>
       {/* Mobile Footer */}
-      <div className="mobile-footer">
-        {!(currentStep === 1) && (
-          <Button variant="text" onClick={handlePrevStep}>
-            Go Back
-          </Button>
-        )}
-        {currentStep != 4 && (
-          <Button
-            variant="contained"
-            onClick={handleNextStep}
-            sx={{ marginLeft: "auto" }}
-          >
-            Next Step
-          </Button>
-        )}
-        {currentStep === 4 && (
-          <Button
-            variant="contained"
-            size="large"
-            onClick={() => {
-              alert("I've been clicked");
-            }}
-            sx={{ backgroundColor: "var(--purplish-blue)", marginLeft: "auto" }}
-          >
-            Confirm
-          </Button>
-        )}
-      </div>
+      {!isFormComplete && (
+        <div className="mobile-footer">
+          {!(currentStep === 1) && (
+            <Button variant="text" onClick={handlePrevStep}>
+              Go Back
+            </Button>
+          )}
+          {currentStep != 4 && (
+            <Button
+              variant="contained"
+              onClick={handleNextStep}
+              sx={{ marginLeft: "auto" }}
+            >
+              Next Step
+            </Button>
+          )}
+          {currentStep === 4 && (
+            <Button
+              variant="contained"
+              size="large"
+              onClick={handleConfirm}
+              sx={{
+                backgroundColor: "var(--purplish-blue)",
+                marginLeft: "auto",
+              }}
+            >
+              Confirm
+            </Button>
+          )}
+        </div>
+      )}
     </>
   );
 };
